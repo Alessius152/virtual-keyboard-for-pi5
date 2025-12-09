32711ab8d1913aaf6e96e82b26a6d35ccf7c0a5e
Alessius152 <stiglianialessio567@gmail.com>
2025-12-09 20:55:55 +0000

    Update server.js
    server

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 // server.js
 //da eseguire sul pi (nel mio caso il pi5)
+// server.js
+// da eseguire sul Pi
+
 const net = require('net');
-const robot = require('robotjs');
+const { keyboard, mouse, Point, Button } = require("@nut-tree-fork/nut-js");
+
+// Impostazioni opzionali (aumentano la velocità)
+keyboard.config.autoDelayMs = 0;
+mouse.config.autoDelayMs = 0;
 
 const server = net.createServer((socket) => {
     console.log('Client connesso');
 
-    socket.on('data', (data) => {
+    socket.on('data', async (data) => {
         try {
             const event = JSON.parse(data.toString());
-            if(event.type === 'keyboard') {
-                robot.keyTap(event.key);
-            } else if(event.type === 'mouse') {
-                robot.moveMouse(event.x, event.y);
-                if(event.button) robot.mouseClick(event.button);
+
+            if (event.type === 'keyboard') {
+                await keyboard.type(event.key);
             }
-        } catch(err) {
+
+            else if (event.type === 'mouse') {
+                await mouse.setPosition(new Point(event.x, event.y));
+
+                if (event.button) {
+                    const btn =
+                        event.button === "left" ? Button.LEFT :
+                        event.button === "right" ? Button.RIGHT :
+                        Button.MIDDLE;
+
+                    await mouse.click(btn);
+                }
+            }
+
+        } catch (err) {
             console.error('Errore parsing:', err);
         }
     });
